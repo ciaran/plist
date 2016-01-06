86d68203077c391ec3b9eba3f19acb0d32e3106e
Ciarán Walsh <git@ciaranwal.sh>
2016-01-06 17:36:53 +0000

    Update package info

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Plist.Mixfile do
 
   def project do
     [app: :plist,
-     version: "0.0.1",
-     description: "An Elixir library to parse files in Apple's binary property list format",
+     version: "0.0.2",
+     description: "An Elixir library to parse files in Apple's property list formats",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -19,15 +19,6 @@ defmodule Plist.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
