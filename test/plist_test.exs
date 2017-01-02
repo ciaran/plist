565b94a552665cb9218f4474aa467aacec9bd0da
Ciarán Walsh <git@ciaranwal.sh>
2017-01-02 16:53:40 +0000

    Update Dict usage

--- test/plist_test.exs
+++ test/plist_test.exs
@@ -5,26 +5,26 @@ defmodule PlistTest do
   test "basic parsing (binary)" do
     plist = parse_fixture("fixture-binary.plist")
 
-    assert Dict.get(plist, "String") == "foobar"
-    assert Dict.get(plist, "Number") == 1234
-    assert Dict.get(plist, "Array") == ["A", "B", "C"]
-    assert Dict.get(plist, "Date") == "2015-11-17 14:00:59 +0000"
-    assert Dict.get(plist, "True") == true
+    assert Map.get(plist, "String") == "foobar"
+    assert Map.get(plist, "Number") == 1234
+    assert Map.get(plist, "Array") == ["A", "B", "C"]
+    assert Map.get(plist, "Date") == "2015-11-17 14:00:59 +0000"
+    assert Map.get(plist, "True") == true
   end
 
   test "basic parsing (xml)" do
     plist = parse_fixture("fixture-xml.plist")
 
-    assert Dict.get(plist, "String") == "foobar"
-    assert Dict.get(plist, "Number") == 1234
-    assert Dict.get(plist, "Float") == 1234.1234
-    assert Dict.get(plist, "Array") == ["A", "B", "C"]
-    assert Dict.get(plist, "Date") == "2015-11-17T14:00:59Z"
-    assert Dict.get(plist, "True") == true
-    assert Dict.get(plist, "Base64") == <<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>
-    assert Dict.get(plist, "EntityEncoded") == "Foo & Bar"
-    assert Dict.get(plist, "UnicσdeKey") == "foobar"
-    assert Dict.get(plist, "UnicodeValue") == "© 2008 – 2016"
+    assert Map.get(plist, "String") == "foobar"
+    assert Map.get(plist, "Number") == 1234
+    assert Map.get(plist, "Float") == 1234.1234
+    assert Map.get(plist, "Array") == ["A", "B", "C"]
+    assert Map.get(plist, "Date") == "2015-11-17T14:00:59Z"
+    assert Map.get(plist, "True") == true
+    assert Map.get(plist, "Base64") == <<0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10>>
+    assert Map.get(plist, "EntityEncoded") == "Foo & Bar"
+    assert Map.get(plist, "UnicσdeKey") == "foobar"
+    assert Map.get(plist, "UnicodeValue") == "© 2008 – 2016"
   end
 
   defp parse_fixture(filename) do
