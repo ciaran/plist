de46066b81b1d66b437eaaf06e2fc27a14ef9d02
Ciarán Walsh <git@ciaranwal.sh>
2018-04-08 17:20:04 +0100

    No need for these to be public

--- lib/xml.ex
+++ lib/xml.ex
@@ -1,8 +1,8 @@
 defmodule Plist.XML do
   require Record
 
-  Record.defrecord(:xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl"))
-  Record.defrecord(:xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl"))
+  Record.defrecordp(:xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl"))
+  Record.defrecordp(:xmlText, Record.extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl"))
 
   def parse(xml) do
     {doc, _} =
