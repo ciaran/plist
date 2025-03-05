52eef813908d51c114ec902f5fdc25707db3b843
rvbiljouw <rb@teller.io>
2025-03-05 14:15:45 +0100

    ensure that double spaced values work in binary plists

--- test/plist_test.exs
+++ test/plist_test.exs
@@ -12,6 +12,7 @@ defmodule PlistTest do
     assert Map.get(plist, "True") == true
     assert Map.get(plist, "SomeUID")["CF$UID"] == 40
     assert Map.get(plist, "") == ""
+    assert Map.get(plist, "DoubleSpaced") == "foo  bar"
   end
 
   test "basic parsing (xml)" do
